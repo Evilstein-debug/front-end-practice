80eda4f34cb1d6f22f553d7817787194ef255be1
evilstein-debug <tejaspathak2005@gmail.com>
2025-06-15 01:27:53 +0530

    add components and custom hooks

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {InputBox} from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState("")
+
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
+  const convert = () => {
+    setConvertedAmount(currencyInfo[to] * amount)
+  }
 
   return (
     <>
      <h1 className='text-3xl'>Currency Convertor</h1>
+     <InputBox />
     </>
   )
 }
