102076a0cc1382cd89315fc2786e46e33c6ae78e
evilstein-debug <tejaspathak2005@gmail.com>
2025-06-21 20:02:46 +0530

    react router init

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-3xl'>React Router</h1>
+    </>
+  )
+}
+
+export default App
